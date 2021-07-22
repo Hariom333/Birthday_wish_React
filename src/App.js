4f87f9bf757cdc231dc384171d5331f11e18ac65
Hariom <rhariom@deaninfotech.com>
2021-07-22 23:02:40 +0530

    birthday wish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from 'react'
+import List from './List'
+import data from './data'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   
+  const [setVal, updateVal] = useState(data);
+ 
+  // console.log({setVal});
+  return ( 
+    <div>
+     
+      <h3>{data.length} birthday to day</h3>
+     
+       <List people = {setVal}/>  
+       <button onClick = {()=> updateVal([]) }>Clear all</button>     
     </div>
-  );
+  )
 }
 
-export default App;
+
+export default App
